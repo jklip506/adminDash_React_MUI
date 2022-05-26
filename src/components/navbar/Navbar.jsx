b488cbdec4f7c97e9e38a395b64452fd6228eb11
jklip506 <96444138+jklip506@users.noreply.github.com>
2022-05-26 15:30:30 -0500

    new commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './navbar.scss';
 
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatIcon from '@mui/icons-material/Chat';
+import ListIcon from '@mui/icons-material/List';
+
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search...' />
+          <SearchIcon className='icon'/>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className='icon'/>
+            English
+          </div>
+          <div className="item">
+            <DarkModeIcon className='icon'/>
+          </div>
+          <div className="item">
+            <FullscreenIcon className='icon'/>
+          </div>
+          <div className="item">
+            <NotificationsIcon className='icon'/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatIcon className='icon'/>
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListIcon className='icon'/>
+          </div>
+          <div className="item">
+            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470"
+             alt="" 
+             className='avatar'
+             />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,76 @@
 import React from 'react';
 import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import CategoryIcon from '@mui/icons-material/Category';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import DescriptionIcon from '@mui/icons-material/Description';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">Sidebar</div>
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">JasonAdmin</span>
+      </div>
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className='icon'/>
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PersonIcon className='icon'/>
+            <span>Users</span>
+          </li>
+          <li> 
+            <CategoryIcon className='icon'/>
+            <span>Products</span>
+          </li>
+          <li>
+            <LocalShippingIcon className='icon'/>
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <QueryStatsIcon className='icon'/>
+            <span>Stats</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <HealthAndSafetyIcon className='icon'/>
+            <span>System Health</span>
+          </li>
+          <li>
+            <DescriptionIcon className='icon'/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className='icon'/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountBoxIcon className='icon'/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className='icon'/>
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
   )
 }
 
